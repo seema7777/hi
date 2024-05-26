478950bf3885d9fd7db79dc873c8c514076329de
seema7777 <85827692+seema7777@users.noreply.github.com>
2024-05-26 21:12:18 +0530

    Update practice.cs

--- practice.cs
+++ practice.cs
@@ -1,6 +1,6 @@
 using System;
 class practice {
-    public static void main(String[] args){
+    public static void Main (String[] args){
         Console.WriteLine("hello world!");
         
     }
